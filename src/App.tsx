a05ce1d6a121efe245a7af6e82d7ad18531ce91e
ali tarichi <tarichiali@gmail.com>
2024-07-04 16:56:14 +0300

    added session and break setters

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { DisplayState } from "./helpers";
 
 const defaultBreakTime = 5 * 60;
 const defaultSessionTime = 25 * 60;
@@ -12,9 +13,25 @@ const interval = 60;
 function App() {
   const [breakTime, setBreakTime] = useState(defaultBreakTime);
   const [sessionTime, setSessionTime] = useState(defaultSessionTime);
-  const [displayState, setDisplayState] = useState<DisplayState>("Session");
+  const [displayState, setDisplayState] = useState<DisplayState>(
+    time: sessionTime,
+    timeType: "Session",
+    timeRunning: false,
+  );
 
-  return <></>;
+  return <>
+    <div className="clock">
+      <div className="setters">
+        <div className="session">
+          <h4 id="session-label"> Seesion Lenght</h4>
+          <TimeSetter />
+        </div>
+        <div className="break"></div>
+        <h4 id="break-label">Break Lenght</h4>
+        <TimeSetter />
+      </div>
+    </div>
+  </>;
 }
 
 export default App;
